500c991e4ba2d5b8fa11851cee3b330b4634877e
acostoya <acostoya@proton.me>
2025-08-15 15:44:24 +0200

    script and config file to automate system configuration

---
+++ init.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+set -e
+
+echo
+cat << "EOF"
+ _____                                                                               _____ 
+( ___ )                                                                             ( ___ )
+ |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | 
+ |   |      ____                    _                  _       _ _         _         |   | 
+ |   |     |  _ \ _   _ _ __  _ __ (_)_ __   __ _     (_)_ __ (_) |_   ___| |__      |   | 
+ |   |     | |_) | | | | '_ \| '_ \| | '_ \ / _` |    | | '_ \| | __| / __| '_ \     |   | 
+ |   |     |  _ <| |_| | | | | | | | | | | | (_| |    | | | | | | |_ _\__ \ | | |    |   | 
+ |   |     |_| \_\\__,_|_| |_|_| |_|_|_| |_|\__, |    |_|_| |_|_|\__(_)___/_| |_|    |   | 
+ |   |                                      |___/                                    |   | 
+ |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| 
+(_____)                                                                             (_____)
+
+EOF
+
+# Updating pakages
+echo "<<<<<<< UPDATING APT PACKAGES >>>>>>>"
+sudo apt update 
+echo
+
+# Upgrading pakages
+echo "<<<<<<< UPGRADING APT PACKAGES >>>>>>>"
+sudo apt update 
+echo
+
+# Installing packages
+echo "<<<<<<< INSTALLING PACKAGES >>>>>>>"
+if [ ! -f "packages.conf" ]; then
+  echo "Error: packages.conf not found!"
+  exit 1
+fi
+source packages.conf
+for package in ${packages[@]}; do
+  echo "INSTALLING PACKAGE: ${package}"
+  sudo apt-get install ${package} -y
+  echo
+done
+
+# Downloading nerd-font
+echo "<<<<<<< INSTALLING NERD-FONT >>>>>>>"
+if [ -d "$HOME/Downloads/JetBrainsMono" ]; then
+  rm -r $HOME/Downloads/JetBrainsMono
+fi
+mkdir -p $HOME/Downloads/JetBrainsMono
+curl -sOL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.tar.xz --output-dir $HOME/Downloads/JetBrainsMono
+tar xf $HOME/Downloads/JetBrainsMono.tar.xz -C $HOME/Downloads/JetBrainsMono
+rm $HOME/Downloads/JetBrainsMono/JetBrainsMono.tar.xz
+mkdir -p $HOME/.local/share/fonts
+mv $HOME/Downloads/JetBrainsMono/* ~/.local/share/fonts
+rm -r $HOME/Downloads/JetBrainsMono
+fc-cache -f
+echo "Nerd-font installed!"
+echo
+
+# Installing node.js
+echo "<<<<<<< INSTALLING NODE.JS >>>>>>>"
+curl -so- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+echo
+
+# Tmux plugin manager
+echo "<<<<<<< INSTALLING TPM >>>>>>>"
+if [ -d "$HOME/.tmux/plugins/tpm" ]; then
+  echo "Repository '$HOME/.tmux/plugins/tpm' already exists. Skipping clone."
+else
+  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+  echo "TPM installed!"
+fi
+echo
+
+# Stowing dotfiles
+echo "<<<<<<< STOWING DOTFILES >>>>>>>"
+cd $HOME/dotfiles
+stow alacritty tmux nvim
+echo "Dotfiles stowed!"
+echo
+
+# Setting alacritty as default terminal
+echo "<<<<<<< SETTING ALACRITTY AS DEFAULT TERMINAL >>>>>>>"
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/alacritty 50
+echo "Alacritty configured as default terminal!"
+echo
+
+# Addint tmux auto-start to .bashrc
+echo "<<<<<<< ADDING TMUX AUTO-START COMMAND TO .bashrc >>>>>>>"
+if ! grep -q 'exec tmux' $HOME/.bashrc; then
+  cat << 'EOF' >> $HOME/.bashrc
+
+# Auto-start tmux
+if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =$HOME screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+  exec tmux
+fi
+EOF
+fi
+echo "TMUX auto-start configured!"
+echo
+
+
+echo "<<<<<<< INSTALLING UV BY ASTRAL >>>>>>>"
+curl -LsSf https://astral.sh/uv/install.sh | sh
