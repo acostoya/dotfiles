8ec3afd0b758603724ea63db39eb7da723b89a9a
acostoya <acostoya@proton.me>
2026-01-09 11:43:18 +0100

    some fixes

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
 set -e
 
 echo ">> Running init.sh script [Arch Linux version] <<"
@@ -16,8 +17,30 @@ fi
 # Stowing dotfiles
 echo "Stowing dotfiles..."
 cd $HOME/dotfiles
-stow hyprland waybar ghostty nvim zellij backgrounds
+stow hyprland waybar ghostty starship nvim zellij backgrounds
 echo "✓ Dotfiles stowed successfully."
 
+# Configuring starship
+echo "Initializing starship in bashrc..."
+if ! grep -q "eval \"\$(starship init bash)\"" ~/.bashrc; then
+  echo 'eval "$(starship init bash)"' >> ~/.bashrc
+fi
+echo "✓ Starship initialized in bashrc."
 
-echo ">> init.sh script run successfully! <<"
+# Setting up SDDM theme
+bash $HOME/dotfiles/sddm-setup.sh catppuccin-mocha-lavender
+
+# Enabling sddm
+echo "Enabling sddm service..."
+sudo systemctl enable sddm
+echo "✓ sddm service enabled successfully."
+
+# Final message and prompt for reboot
+echo ">> init.sh script run successfully! Do you want to reboot now? (y/n) <<"
+read -r answer
+if [[ $answer == "y" || $answer == "Y" ]]; then
+  echo "Rebooting system..."
+  sudo reboot
+else
+  echo "Reboot canceled. Please reboot manually later."
+fi
