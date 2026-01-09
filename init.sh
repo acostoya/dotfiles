3728ddde395bba8c5e21d4a0b902193b6390e905
acostoya <acostoya@proton.me>
2026-01-09 10:47:56 +0100

    some fixes

--- init.sh
+++ init.sh
@@ -8,23 +8,16 @@ echo ">> Running init.sh script [Arch Linux version] <<"
 cd $HOME/dotfiles
 if [ -f "packages.conf" ]; then
   source packages.conf
-  for package in ${packages[@]}; do
-    echo "Installing ${package}..."
-    sudo pacman -Sy ${package} > /dev/null
-    echo "✓ ${package} installed successfully."
-  done
-fi
-
-# Installing tmux plugin manager
-if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-  echo "Installing tmux plugin manager..."
-  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm > /dev/null
+  echo "Installing all packages..."
+  sudo pacman -Sy ${packages[@]}
+  echo "✓ All packages installed successfully."
 fi
 
 # Stowing dotfiles
 echo "Stowing dotfiles..."
 cd $HOME/dotfiles
-stow hyprland waybar ghostty nvim zellij backgrounds desktop
+stow hyprland waybar ghostty nvim zellij backgrounds
+echo "✓ Dotfiles stowed successfully."
 
 
 echo ">> init.sh script run successfully! <<"
