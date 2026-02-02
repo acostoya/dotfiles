bddeef25cc6c7a5b4ccf69572891a26bd2dbb877
acostoya <acostoya@proton.me>
2026-02-02 10:42:23 +0100

    minor fixes to application files

--- init.sh
+++ init.sh
@@ -4,9 +4,9 @@
 set -e
 
 echo ">> Running init.sh script [Arch Linux version] <<"
+cd $HOME/dotfiles
 
 # Installing packages.conf packages
-cd $HOME/dotfiles
 if [ -f "packages.conf" ]; then
   source packages.conf
   echo "Installing all packages..."
@@ -16,9 +16,14 @@ fi
 
 # Stowing dotfiles
 echo "Stowing dotfiles..."
-cd $HOME/dotfiles
 stow hyprland waybar ghostty starship nvim zellij backgrounds
 echo "✓ Dotfiles stowed successfully."
+
+# Creating symlinks for application files
+echo "Creating symlinks for application files..."
+mkdir -p "$HOME/.local/share/applications"
+ln -sf "$HOME/dotfiles/applications/freeview.desktop" "$HOME/.local/share/applications/freeview.desktop"
+echo "✓ Application files symlinked successfully."
 
 # Configuring starship
 echo "Initializing starship in bashrc..."
@@ -33,7 +38,14 @@ sudo systemctl enable gdm
 echo "✓ gdm service enabled successfully."
 
 # Setting up NVIDIA drivers
-bash $HOME/dotfiles/nvidia-setup.sh
+echo "Do you want to set up NVIDIA drivers? (y/n)"
+read -r answer
+if [[ $answer == "y" || $answer == "Y" ]]; then
+  bash $HOME/dotfiles/nvidia-setup.sh
+  echo "✓ NVIDIA drivers set up successfully."
+else
+  echo "NVIDIA driver setup skipped."
+fi
 
 # Final message and prompt for reboot
 echo ">> init.sh script run successfully! Do you want to reboot now? (y/n) <<"
