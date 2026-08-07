07b4e429c2072d7a13851ee1f14f03805070f51f
acostoya <alejandro@zroimaging.com>
2026-08-07 12:53:56 +0200

    updated yamlfmt args in conform to retain empty lines

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -4,9 +4,14 @@ return {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "ruff_organize_imports", "ruff_format", "ruff_fix" },
-			r = {"air"},
+			r = { "air" },
 			yaml = { "yamlfmt" },
 			json = { "prettier" },
+		},
+		formatters = {
+			yamlfmt = {
+				prepend_args = { "-formatter", "retain_line_breaks_single=true" },
+			},
 		},
 	},
 	keys = {
